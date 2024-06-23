442a6ce89e7e8c04dfba62fc3c6924f3185550d6
Utkarsh Sinha <164975503+UtkarshDevv@users.noreply.github.com>
2024-06-24 03:06:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
    build: {
     rollupOptions: {
-      external: ['/src/app.jsx'], // Add any other modules or files here
+      external: ['/src/index.html'], // Add any other modules or files here
     },
   },
 server: {

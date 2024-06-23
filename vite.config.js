9d8326ea26aebc204fa4d98e55d2162acbea8dcd
UtkarshDevv <utkarshsinha2122@gmail>
2024-06-24 01:00:38 +0530

    modified viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 server: {
-<<<<<<< HEAD
 host: true,
 strictPort: true,
 port: 8000,
-=======
-  host:true,
-    port: process.env.PORT || 3000
-
->>>>>>> ce0e642225aa32531435f31e9c0ba60461c1467b
 }
 })

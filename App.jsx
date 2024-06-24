c717bc4e250e2cacde6b69ca549c0be300b39bc5
Utkarsh Sinha <164975503+UtkarshDevv@users.noreply.github.com>
2024-06-24 12:54:37 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ import ScheduleCard from './components/pages/ScheduleCard'
 import About from './components/pages/About'
 import Footer from './components/Footer/Footer'
 import ContactPage from './components/pages/ContactPage'
-import {BrowserRouter,  Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (

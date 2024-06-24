b77824e63845f4e81578a9196ec62dd71b50feb0
Utkarsh Sinha <164975503+UtkarshDevv@users.noreply.github.com>
2024-06-24 13:13:08 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -10,7 +10,7 @@ import {BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    <BrowserRouter>
+    
     <div>
     <Navebar />
         <Routes>
@@ -21,7 +21,7 @@ function App() {
 
         </Routes>
     </div>
-    </BrowserRouter>
+    
     </>
   )
 }
